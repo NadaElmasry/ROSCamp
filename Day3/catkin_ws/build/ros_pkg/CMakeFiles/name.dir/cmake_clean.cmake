36e89dbc53665aa22ad14c46caa3d8e5eab9b6ea
NadaElmasry <nadaahmedelmasry20@gmail.com>
2020-08-14 18:15:19 +0200

    creating a package andadding myself as the maintainer

---
+++ Day3/catkin_ws/build/ros_pkg/CMakeFiles/name.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/samar/catkin_ws/devel/lib/ros_pkg/name"
+  "/home/samar/catkin_ws/devel/lib/ros_pkg/name.pdb"
+  "CMakeFiles/name.dir/src/name.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/name.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
